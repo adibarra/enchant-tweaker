a36597c0e14fb1a7d867d25c28ae2aa95caa141b
adibarra <93070681+adibarra@users.noreply.github.com>
2023-04-19 00:07:22 -0500

    Implement NoSoulSpeedBacklashMixin

---
+++ src/main/java/com/adibarra/enchanttweaker/mixin/enchant/custom/NoSoulSpeedBacklashMixin.java
@@ -0,0 +1,23 @@
+package com.adibarra.enchanttweaker.mixin.enchant.custom;
+
+import com.adibarra.enchanttweaker.EnchantTweaker;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(value=LivingEntity.class, priority=1543)
+public abstract class NoSoulSpeedBacklashMixin {
+
+	/**
+	 * @description Disable soul speed armor damage when moving.
+	 * @environment Server
+	 */
+	@ModifyConstant(method="addSoulSpeedBoostIfNeeded()V", constant=@Constant(intValue=1))
+	private int removeBacklash(int original) {
+		if(EnchantTweaker.isEnabled() && EnchantTweaker.getConfig().getOrDefault("no_soul_speed_backlash", true)) {
+			return 0;
+		}
+		return original;
+	}
+}
