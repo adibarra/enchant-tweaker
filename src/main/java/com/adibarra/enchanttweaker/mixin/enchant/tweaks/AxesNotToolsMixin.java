61babff516b8d3f0532d7ee2d5a160b29af02116
adibarra <93070681+adibarra@users.noreply.github.com>
2023-04-20 16:17:52 -0500

    Fix AxesNotToolsMixin

--- src/main/java/com/adibarra/enchanttweaker/mixin/enchant/tweaks/AxesNotToolsMixin.java
+++ src/main/java/com/adibarra/enchanttweaker/mixin/enchant/tweaks/AxesNotToolsMixin.java
@@ -1,22 +1,25 @@
 package com.adibarra.enchanttweaker.mixin.enchant.tweaks;
 
 import com.adibarra.enchanttweaker.EnchantTweaker;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
+/**
+ * @description Remove extra self-damage from axes when used as a weapon.
+ * @environment Server
+ */
 @Mixin(value=MiningToolItem.class, priority=1543)
 public abstract class AxesNotToolsMixin {
-
-	/**
-	 * @description Remove extra self-damage from axes when used as a weapon.
-	 * @environment Server
-	 */
 	@ModifyConstant(method="postHit", constant=@Constant(intValue=2))
-	public int modifySelfDamage(int original) {
+	public int modifySelfDamage(int original, ItemStack stack) {
 		if(EnchantTweaker.isEnabled() && EnchantTweaker.getConfig().getOrDefault("axes_not_tools", true)) {
-			return 1;
+			if(stack.getItem() instanceof AxeItem) {
+				return 1;
+			}
 		}
 		return original;
 	}
