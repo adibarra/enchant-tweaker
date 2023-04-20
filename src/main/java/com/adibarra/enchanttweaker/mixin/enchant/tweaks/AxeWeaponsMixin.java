a9780591a10296f932ccdebd641d86d1540c30e6
adibarra <93070681+adibarra@users.noreply.github.com>
2023-04-20 16:38:51 -0500

    Implement AxeWeaponsMixin

---
+++ src/main/java/com/adibarra/enchanttweaker/mixin/enchant/tweaks/AxeWeaponsMixin.java
@@ -0,0 +1,30 @@
+package com.adibarra.enchanttweaker.mixin.enchant.tweaks;
+
+import com.adibarra.enchanttweaker.EnchantTweaker;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @description Allow axes to be enchanted with fire aspect, knockback, and looting.
+ * @environment Server
+ */
+@Mixin(value=Enchantment.class, priority=1543)
+public abstract class AxeWeaponsMixin {
+	@Inject(method="isAcceptableItem", at=@At("HEAD"), cancellable=true)
+	public void axeWeapons(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+		if(EnchantTweaker.isEnabled() && EnchantTweaker.getConfig().getOrDefault("axe_weapons", true)) {
+			if(stack.getItem() instanceof AxeItem) {
+				Enchantment enchantment = (Enchantment) (Object) this;
+				if(enchantment == Enchantments.FIRE_ASPECT || enchantment == Enchantments.KNOCKBACK || enchantment == Enchantments.LOOTING) {
+					cir.setReturnValue(true);
+				}
+			}
+		}
+	}
+}
