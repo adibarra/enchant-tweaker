77da6334faa3811ff644a88aa55b5294a515e78a
adibarra <93070681+adibarra@users.noreply.github.com>
2023-04-21 04:43:51 -0500

    Remove @override

--- src/main/java/com/adibarra/enchanttweaker/mixin/server/tweaks/LoyalTridentsVoidMixin.java
+++ src/main/java/com/adibarra/enchanttweaker/mixin/server/tweaks/LoyalTridentsVoidMixin.java
@@ -1,7 +1,6 @@
 package com.adibarra.enchanttweaker.mixin.server.tweaks;
 
 import com.adibarra.enchanttweaker.EnchantTweaker;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.projectile.ProjectileEntity;
@@ -10,13 +9,16 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * @description Lets tridents with Loyalty enchant return when thrown into the void.
  * @environment Server
  */
 @Mixin(value=TridentEntity.class, priority=1543)
-public abstract class LoyalTridentsVoidMixin extends Entity {
+public abstract class LoyalTridentsVoidMixin extends ProjectileEntity {
 
     @Final @Shadow
     private static TrackedData<Byte> LOYALTY;
@@ -25,18 +27,19 @@ public abstract class LoyalTridentsVoidMixin extends Entity {
     @Shadow
     private boolean dealtDamage;
 
-    protected LoyalTridentsVoidMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+    protected LoyalTridentsVoidMixin(EntityType<ProjectileEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    @Override
-    protected void tickInVoid() {
+    @Inject(method="tick", at=@At("HEAD"))
+    private void tick(CallbackInfo ci) {
         if(EnchantTweaker.isEnabled() && EnchantTweaker.getConfig().getOrDefault("loyal_tridents_void", true)) {
-            if(dataTracker.get(LOYALTY) > 0) {
-                dealtDamage = true;
-                return;
+            if(dataTracker.get(LOYALTY) > 0 && !this.dealtDamage) {
+                if(this.getY() <= this.world.getBottomY() && !this.world.isClient()) {
+                    this.dealtDamage = true;
+                    this.setVelocity(0, 0, 0);
+                }
             }
         }
-        super.tickInVoid();
     }
 }
