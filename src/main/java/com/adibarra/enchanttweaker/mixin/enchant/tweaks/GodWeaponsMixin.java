13f662d1a2ed6c09e96a0ba5ff1e76061856497a
adibarra <93070681+adibarra@users.noreply.github.com>
2023-04-20 23:06:11 -0500

    Implement GodWeaponsMixin

---
+++ src/main/java/com/adibarra/enchanttweaker/mixin/enchant/tweaks/GodWeaponsMixin.java
@@ -0,0 +1,24 @@
+package com.adibarra.enchanttweaker.mixin.enchant.tweaks;
+
+import com.adibarra.enchanttweaker.EnchantTweaker;
+import net.minecraft.enchantment.DamageEnchantment;
+import net.minecraft.enchantment.Enchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @description Allow weapons to be enchanted with multiple damage enchantments.
+ * @environment Server
+ */
+@Mixin(value=DamageEnchantment.class, priority=1543)
+public abstract class GodWeaponsMixin {
+	@Inject(method="canAccept", at=@At("HEAD"), cancellable=true)
+	private void allowGodWeapons(Enchantment other, CallbackInfoReturnable<Boolean> cir) {
+		if(EnchantTweaker.isEnabled() && EnchantTweaker.getConfig().getOrDefault("god_weapons", true)) {
+			if(other instanceof DamageEnchantment)
+				cir.setReturnValue(true);
+		}
+	}
+}
