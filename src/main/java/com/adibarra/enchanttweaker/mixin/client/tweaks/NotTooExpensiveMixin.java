a99f9bcdd4c401f0b59c1372c8775814383891e4
adibarra <93070681+adibarra@users.noreply.github.com>
2023-04-21 02:55:29 -0500

    Implement NotTooExpensiveMixin (server + client)

---
+++ src/main/java/com/adibarra/enchanttweaker/mixin/client/tweaks/NotTooExpensiveMixin.java
@@ -0,0 +1,27 @@
+package com.adibarra.enchanttweaker.mixin.client.tweaks;
+
+import com.adibarra.enchanttweaker.EnchantTweaker;
+import com.adibarra.enchanttweaker.ETUtils;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.ingame.AnvilScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+/**
+ * @description Anvils no longer say "Too Expensive!"
+ * @environment Client
+ */
+@Environment(EnvType.CLIENT)
+@Mixin(value=AnvilScreen.class, priority=1543)
+public abstract class NotTooExpensiveMixin {
+	@ModifyConstant(method="drawForeground", constant=@Constant(intValue=40))
+	private int notTooExpensiveClient(int previous) {
+		if(EnchantTweaker.isEnabled() && EnchantTweaker.getConfig().getOrDefault("not_too_expensive", true)) {
+			int max_cost = EnchantTweaker.getConfig().getOrDefault("max_level_cost", Integer.MAX_VALUE);
+			return ETUtils.clamp(max_cost, 0, Integer.MAX_VALUE);
+		}
+		return previous;
+	}
+}
---
+++ src/main/java/com/adibarra/enchanttweaker/mixin/server/tweaks/NotTooExpensiveMixin.java
@@ -0,0 +1,24 @@
+package com.adibarra.enchanttweaker.mixin.server.tweaks;
+
+import com.adibarra.enchanttweaker.EnchantTweaker;
+import com.adibarra.enchanttweaker.ETUtils;
+import net.minecraft.screen.*;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+/**
+ * @description Anvils no longer say "Too Expensive!"
+ * @environment Server
+ */
+@Mixin(value=AnvilScreenHandler.class, priority=1543)
+public abstract class NotTooExpensiveMixin {
+	@ModifyConstant(method="updateResult", constant=@Constant(intValue=40, ordinal=2))
+	private int notTooExpensive(int previous) {
+		if(EnchantTweaker.isEnabled() && EnchantTweaker.getConfig().getOrDefault("not_too_expensive", true)) {
+			int max_cost = EnchantTweaker.getConfig().getOrDefault("max_level_cost", Integer.MAX_VALUE);
+			return ETUtils.clamp(max_cost, 0, Integer.MAX_VALUE);
+		}
+		return previous;
+	}
+}
