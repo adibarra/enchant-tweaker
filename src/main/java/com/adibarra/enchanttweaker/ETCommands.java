c1b08463163f78d37046286a226deaf599067101
adibarra <93070681+adibarra@users.noreply.github.com>
2023-05-01 16:23:43 -0500

    Refactor ETCommands

--- src/main/java/com/adibarra/enchanttweaker/ETCommands.java
+++ src/main/java/com/adibarra/enchanttweaker/ETCommands.java
@@ -1,93 +1,85 @@
 package com.adibarra.enchanttweaker;
 
-import com.adibarra.utils.Utils;
+import com.adibarra.utils.ADBrigadier;
+import com.adibarra.utils.ADText;
+import com.mojang.brigadier.tree.CommandNode;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.ClickEvent;
-import net.minecraft.text.HoverEvent;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class ETCommands {
 
-	private static final String[] ALIASES = new String[] { "enchanttweaker", "et" };
+    private record Command(String description, Supplier<LiteralCommandNode<ServerCommandSource>> node) { }
 
-	private static final List<LiteralCommandNode<ServerCommandSource>> COMMAND_NODES = List.of(
-		CommandManager.literal("reload")
-			.requires(source -> source.hasPermissionLevel(2))
-			.executes(context -> {
-				ETMixinPlugin.reloadConfig();
-				context.getSource().sendFeedback(
-					Utils.joinText(new Text[] {
-						Text.literal("[Enchant Tweaker]").formatted(Formatting.GREEN),
-						Text.literal(" Config Reloaded!")
-					}), false);
-				return 1;
-			}).build()
-	);
+    private static final List<Command> COMMANDS = new ArrayList<>();
+
+    static {
+        COMMANDS.add(
+            new Command("Reloads the config file.", () ->
+                CommandManager.literal("reload")
+                    .requires(source -> source.hasPermissionLevel(2))
+                    .executes(context -> {
+                        ETMixinPlugin.reloadConfig();
+                        Text[] out = new Text[] {
+                            Text.literal(EnchantTweaker.PREFIX).formatted(Formatting.GREEN),
+                            Text.literal("Config Reloaded!")
+                        };
+                        context.getSource().sendFeedback(ADText.joinText(out), false);
+                        return 1;
+                    }).build())
+        );
+
+        COMMANDS.add(
+            new Command("Shows this help message.", () ->
+                CommandManager.literal("help")
+                    .requires(source -> source.hasPermissionLevel(2))
+                    .executes(context -> {
+                        List<Text> out = new ArrayList<>();
+                        out.add(Text.literal(EnchantTweaker.PREFIX).formatted(Formatting.GREEN));
+                        for (Command command : COMMANDS) {
+                            out.add(Text.literal("\n")
+                                .append(ADText.joinText(new Text[] {
+                                    ADText.buildCmdLink("/et " + command.node.get().getLiteral()),
+                                    Text.literal(" - " + command.description)
+                                })));
+                        }
+                        context.getSource().sendFeedback(ADText.joinText(out), false);
+                        return 1;
+                    }).build())
+        );
+  }
 
 	public static void registerCommands() {
-		CommandRegistrationCallback.EVENT.register((commandDispatcher, registryAccess, environment) -> {
+        // Build base nodes
+        CommandRegistrationCallback.EVENT.register((commandDispatcher, registryAccess, environment) -> {
+            CommandNode<ServerCommandSource> baseNode = CommandManager.literal("et")
+                .requires(source -> source.hasPermissionLevel(2))
+                .executes(context -> {
+                    Text[] out = new Text[] {
+                        Text.literal(EnchantTweaker.PREFIX).formatted(Formatting.GREEN),
+                        Text.literal("Try running ").formatted(Formatting.GRAY),
+                        ADText.buildCmdLink("/et help"),
+                        Text.literal(" for more information.").formatted(Formatting.GRAY)
+                    };
+                    context.getSource().sendFeedback(ADText.joinText(out), false);
+                    return 1;
+                }).build();
 
-			// Register all commands to all aliases
-			for (String alias : ALIASES) {
-				for (LiteralCommandNode<ServerCommandSource> node : COMMAND_NODES) {
-					commandDispatcher.register(CommandManager.literal(alias).then(node));
-				}
+            // Register subcommands to base node
+            for (Command command : COMMANDS) {
+                baseNode.addChild(command.node.get());
+            }
 
-				// Base command should show help message
-				commandDispatcher.register(
-					CommandManager.literal(alias)
-						.requires(source -> source.hasPermissionLevel(2))
-						.executes(context -> {
-							context.getSource().sendFeedback(
-								Utils.joinText(new Text[] {
-									Text.literal("[Enchant Tweaker] ").formatted(Formatting.GREEN),
-									Text.literal("Try running ").formatted(Formatting.GRAY),
-									Text.literal("/et help ")
-										.setStyle(Style.EMPTY
-											.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/et help"))
-											.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to run /et help")))
-									),
-									Text.literal("for more information.").formatted(Formatting.GRAY)
-								}), false);
-							return 1;
-						})
-				);
-
-				// Generate and register Help command
-				commandDispatcher.register(
-					CommandManager.literal(alias)
-						.then(CommandManager.literal("help")
-							.requires(source -> source.hasPermissionLevel(2))
-							.executes(context -> {
-								context.getSource().sendFeedback(
-									Utils.joinText(new Text[] {
-										Text.literal("[Enchant Tweaker]\n").formatted(Formatting.GREEN),
-										Text.literal("/et reload ")
-											.setStyle(Style.EMPTY
-												.withColor(Formatting.AQUA)
-												.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/et reload"))
-												.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to run /et reload")))
-											),
-										Text.literal("- Reloads the config file.\n"),
-										Text.literal("/et help ")
-											.setStyle(Style.EMPTY
-												.withColor(Formatting.AQUA)
-												.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/et help"))
-												.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to run /et help")))
-											),
-										Text.literal("- Shows this message."),
-									}), false);
-								return 1;
-							}))
-				);
-			}
-		});
+            // Register base nodes to root
+            commandDispatcher.getRoot().addChild(baseNode);
+            commandDispatcher.getRoot().addChild(ADBrigadier.buildAlias("enchanttweaker", baseNode));
+        });
 	}
 }
